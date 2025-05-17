f977cb40e48b9169fb60888ec700dee66192993b
ngotuanminh1 <ngotuanminh2689@gmail.com>
2025-05-17 16:02:23 +0700

    New

--- app/Cron/botBroadcast.js
+++ app/Cron/botBroadcast.js
@@ -13,7 +13,9 @@ module.exports = function (redT) {
 
             const userNames = [
                 "minhng5616", "hoanganh98", "trungkts", "bichhanh99", "vietanhx1",
-                "dongduong56", "duyvip79", "khachmoi01", "son12345", "thaobx08"
+                "dongduong56", "duyvip79", "khachmoi01", "son12345", "thaobx08",
+                "laicamxe123", "toananh6", "ngonnguyenanh9x", "maianhtrieu", "qrepewa2x12",
+                "ykiung6868", "dungraxiu12", "631anhtuan", "qweer23", "giungki001"
             ];
 
             const user = userNames[Math.floor(Math.random() * userNames.length)];
