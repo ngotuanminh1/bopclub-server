c32d6ccf5de8334bd34a7adecd8ed42d73a907da
ngotuanminh1 <ngotuanminh2689@gmail.com>
2025-05-18 01:56:29 +0700

    Made with cocos

--- public2/main.a6845.js
+++ public2/main.a6845.js
@@ -55,9 +55,22 @@ window.boot = function () {
     }
 
     function setLoadingDisplay () {
-  // Disable default splash
-  return;
-}
+        // Loading splash scene
+        var splash = document.getElementById('splash');
+        var progressBar = splash.querySelector('.progress-bar span');
+        cc.loader.onProgress = function (completedCount, totalCount, item) {
+            var percent = 100 * completedCount / totalCount;
+            if (progressBar) {
+                progressBar.style.width = percent.toFixed(2) + '%';
+            }
+        };
+        splash.style.display = 'block';
+        progressBar.style.width = '0%';
+
+        cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
+            splash.style.display = 'none';
+        });
+    }
 
     var onStart = function () {
         cc.loader.downloader._subpackages = settings.subpackages;
